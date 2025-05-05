2d69baa2cd3f641886fc5c2a014167651700101a
Daniel Cabrera <ldanielcabreral@gmail.com>
2025-05-04 22:03:22 -0500

    Crear un banner personalizado para mostrar los titulos

--- functions/functions-acf-options.php
+++ functions/functions-acf-options.php
@@ -2,15 +2,15 @@
 if( function_exists('acf_add_options_page') ) {
     
     acf_add_options_page(array(
-        'page_title'    => 'FR Ajustes Generales',
-        'menu_title'    => 'FR Ajustes',
+        'page_title'    => 'Ajustes Generales',
+        'menu_title'    => 'Ajustes',
         'menu_slug'     => 'theme-general-settings',
         'capability'    => 'edit_posts',
         'redirect'      => false
     ));
     
     acf_add_options_sub_page(array(
-        'page_title'    => 'FR Ajustes de Encabezado',
+        'page_title'    => 'Ajustes de Encabezado',
         'menu_title'    => 'Header',
         'parent_slug'   => 'theme-general-settings',
     ));
