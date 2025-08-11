e0c38bec56965a67411e5571e44eede4b343684e
Daniel Cabrera <ldanielcabreral@gmail.com>
2025-08-10 19:46:32 -0500

    Se agrega el padding a material didactico y salud bucal

--- blocks/patients-materials/patients-materials.php
+++ blocks/patients-materials/patients-materials.php
@@ -10,7 +10,7 @@ if (!$materials) {
 foreach ($materials as $material) {
  $material_title = $material['title'];
 ?>
- <h2 class="text-2xl font-bold text-center mb-10"><?= $material_title; ?></h2>
+ <h2 class="text-2xl font-bold text-center my-10"><?= $material_title; ?></h2>
  <?php
  $cards = $material['cards'];
  ?>
